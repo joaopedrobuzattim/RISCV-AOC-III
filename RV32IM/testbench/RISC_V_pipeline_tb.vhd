d0960d1e3faa917992e9ea1f987977bbe0648e38
Bruno <bruno.spies@ecomp.ufsm.br>
2024-10-31 13:15:57 -0300

    refactoring files for synthesis

--- RV32IM/testbench/RISC_V_pipeline_tb.vhd
+++ RV32IM/testbench/RISC_V_pipeline_tb.vhd
@@ -2,57 +2,77 @@ library ieee;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity RISC_V_pipeline_tb  is
+entity RISC_V_pipeline_tb is
 end RISC_V_pipeline_tb;
 
 architecture behavioral of RISC_V_pipeline_tb is
 
-    constant RARS_INSTRUCTION_OFFSET    : std_logic_vector(31 downto 0) := x"00400000"; -- begining addresse of instruction in risc v
-    constant RARS_DATA_OFFSET           : std_logic_vector(31 downto 0) := x"10010000"; -- begining addresse of data in risc v
+    constant RARS_INSTRUCTION_OFFSET : std_logic_vector(31 downto 0) := std_logic_vector(to_unsigned(16#00400000#, 32));
+    constant RARS_DATA_OFFSET        : std_logic_vector(31 downto 0) := std_logic_vector(to_unsigned(16#10010000#, 32));
+
+
 
     signal clock                        : std_logic := '0';
     signal reset                        : std_logic;
     signal data_out_mem                 : std_logic_vector (31 downto 0);
 
-    -- Instruction Memory Signals
-    signal inst_mem_addr     : std_logic_vector (31 downto 0);
-    signal inst_mem_o_data   : std_logic_vector (31 downto 0);
-    signal inst_mem_i_data   : std_logic_vector (31 downto 0);
+    -- Sinais da Memória de Instruções
+    signal inst_mem_addr                : std_logic_vector (31 downto 0);
+    signal inst_mem_o_data              : std_logic_vector (31 downto 0);
+    signal inst_mem_i_data              : std_logic_vector (31 downto 0);
 
-    -- Data Memory Signals
-    signal data_mem_addr    : std_logic_vector (31 downto 0);
-    signal data_mem_o_data  : std_logic_vector (31 downto 0);
-    signal data_mem_i_data  : std_logic_vector (31 downto 0);
-    signal data_mem_wr      : std_logic;
+    -- Sinais da Memória de Dados
+    signal data_mem_addr                : std_logic_vector (31 downto 0);
+    signal data_mem_o_data              : std_logic_vector (31 downto 0);
+    signal data_mem_i_data              : std_logic_vector (31 downto 0);
+    signal data_mem_wr                  : std_logic;
 
-component RISC_V_pipeline_top
-    port (
-        -- Clock and Reset
-        clk              : in  std_logic;
-        rst_n            : in  std_logic;
-        
-        -- Instruction Memory Ports
-        inst_mem_addr_o  : out std_logic_vector(31 downto 0);  
-        inst_mem_data_i  : in  std_logic_vector(31 downto 0);  
-        inst_mem_data_o  : out std_logic_vector(31 downto 0); 
-        
-        -- Data Memory Ports
-        data_mem_addr_o  : out std_logic_vector(31 downto 0);  
-        data_mem_data_i  : in  std_logic_vector(31 downto 0);  
-        data_mem_data_o  : out std_logic_vector(31 downto 0);  
-        data_mem_wr_o    : out std_logic
-    );
+    signal const_zero : std_logic := '0';
 
-end component;
+    -- Declaração do componente Memory
+    component Memory
+        generic (
+            SIZE            : integer := 32;                -- Profundidade da memória
+            START_ADDRESS   : std_logic_vector(31 downto 0) := (others => '0');  -- Endereço inicial mapeado para 0x00000000
+            imageFileName   : string := "UNUSED"            -- Nome do arquivo com o conteúdo da memória
+        );
+        port (
+            clock           : in  std_logic;
+            MemWrite        : in  std_logic;
+            address         : in  std_logic_vector(31 downto 0);
+            data_i          : in  std_logic_vector(31 downto 0);
+            data_o          : out std_logic_vector(31 downto 0)
+        );
+    end component;
 
+    -- Declaração do componente RISC_V_pipeline_top
+    component RISC_V_pipeline_top
+        port (
+            -- Clock e Reset
+            clk              : in  std_logic;
+            rst_n            : in  std_logic;
+            
+            -- Portas da Memória de Instruções
+            inst_mem_addr_o  : out std_logic_vector(31 downto 0);  
+            inst_mem_data_i  : in  std_logic_vector(31 downto 0);  
+            inst_mem_data_o  : out std_logic_vector(31 downto 0); 
+            
+            -- Portas da Memória de Dados
+            data_mem_addr_o  : out std_logic_vector(31 downto 0);  
+            data_mem_data_i  : in  std_logic_vector(31 downto 0);  
+            data_mem_data_o  : out std_logic_vector(31 downto 0);  
+            data_mem_wr_o    : out std_logic
+        );
+    end component;
 
 begin
 
+    -- Geração de clock e reset
     clock <= not clock after 2.5 ns;
     reset <= '1', '0' after 7 ns;
 
-
-    INSTRUCTION_MEMORY: entity work.Memory(behavioral)
+    -- Instância da Memória de Instruções
+    INSTRUCTION_MEMORY: Memory
         generic map (
             SIZE            => 100,
             START_ADDRESS   => RARS_INSTRUCTION_OFFSET, 
@@ -60,14 +80,14 @@ begin
         )
         port map (
             clock           => clock,
-            MemWrite        => '0',
+            MemWrite        => const_zero,
             address         => inst_mem_addr,    
             data_i          => inst_mem_i_data,
             data_o          => inst_mem_o_data
         );
         
-        
-    DATA_MEMORY: entity work.Memory(behavioral)
+    -- Instância da Memória de Dados
+    DATA_MEMORY: Memory
         generic map (
             SIZE            => 100,
             START_ADDRESS   => RARS_DATA_OFFSET,
@@ -81,17 +101,18 @@ begin
             data_o          => data_mem_o_data
         );  
 
-    RISCV_PIPELINE_TOP:    entity work.RISC_V_pipeline_top
+    -- Instância do RISC_V_pipeline_top
+    RISCV_PIPELINE_TOP: RISC_V_pipeline_top
         port map (
             clk             => clock,
             rst_n           => reset,
             
-            -- Instruction Memory Ports         
+            -- Portas da Memória de Instruções         
             inst_mem_addr_o =>   inst_mem_addr,
             inst_mem_data_i =>   inst_mem_o_data,
             inst_mem_data_o =>   inst_mem_i_data,
     
-            -- Data Memory Ports
+            -- Portas da Memória de Dados
             data_mem_addr_o =>   data_mem_addr,
             data_mem_data_i =>   data_mem_o_data,
             data_mem_data_o =>   data_mem_i_data,
@@ -99,3 +120,4 @@ begin
         );
 
 end behavioral;
+
